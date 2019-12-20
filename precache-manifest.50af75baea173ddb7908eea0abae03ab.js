f00a0b2acb799f1e74033d5f4db91932abbd9290
Elizaveta_Cheptsova <cheptsova.liza@mail.ru>
2019-12-20 15:31:38 +0300

    Updates

---
+++ precache-manifest.50af75baea173ddb7908eea0abae03ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd7d4e42515f4414ddbc4e40ac78b2fd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9589fc2ecee9067e814c",
+    "url": "/portfolio/static/css/main.6ae8e9cd.chunk.css"
+  },
+  {
+    "revision": "0427c4a7b4acf6d96b92",
+    "url": "/portfolio/static/js/2.585806fc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.585806fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9589fc2ecee9067e814c",
+    "url": "/portfolio/static/js/main.6e15eb78.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  }
+]);
